a6994acc6cd4489df136fd56a73865308043c17f
Omerpq <omerpq@gmail.com>
2025-08-23 19:32:35 +0500

    test: mock AsyncStorage in Jest setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,3 +5,6 @@ Object.defineProperty(globalThis as any, "__ExpoImportMetaRegistry", {
   value: { getValue: () => undefined },
   configurable: true,
 });
+jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
+
+jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
